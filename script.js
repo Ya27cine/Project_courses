b23a04fc3aa76e3a43ff97e77c8251a92c2855d4
Khelifa Yassine <prostam27@gmail.com>
2020-12-06 17:49:04 +0100

    show course on html page

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 console.log("FireStore !")
+const myList = document.querySelector('ul');
+
+add = course => {
+    myList.innerHTML += "<li class='list-group-item'> <h3> " +
+        course.title +
+        "</h3> <small>" + course.created_at.toDate() + "</small> </li>";
+}
+
 
 db.collection("courses").get()
     .then(res => res.docs.forEach(course => {
-        console.log(course.data())
+        add(course.data());
     }))
     .catch(err => console.log(err))
+
+
+
+class Course {
+
+    constructor() {
+        this.html = document.createElement('li').classList.add('list-group-item')
+        this.title = '';
+        this.created_at = null;
+    }
+
+    create() {
+        console.log(this.html.append(this.nodeTitle())
+            .append(this.setCreatedAt()));
+    }
+
+    nodeTitle() {
+        let el = document.createElement('h3');
+        el.innerText = this.title;
+        return el;
+    }
+
+    nodeCreatedAt() {
+        let el = document.createElement('small');
+        el.innerText = this.created_at;
+        return el;
+    }
+
+    setTitle(title) { this.title = title; }
+    setCreatedAt(create_at) { this.created_at = create_at; }
+
+}
